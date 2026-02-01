35b7419e7bff16b66958387b95d937fda2cfe2b3
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-02-01 23:08:08 +0100

    feat: Add Flight and Reservation models

--- IOS-Development/GestionDuTransport/Sources/GestionDuTransport/Models/Flight.swift
+++ IOS-Development/GestionDuTransport/Sources/GestionDuTransport/Models/Flight.swift
@@ -3,14 +3,46 @@ import Foundation
 class Flight {
     private let number: String
     private let date: Date
+    private(set) var status: FlightStatus = FlightStatus.closed
     private var staffMembers: [Staff] = []
     private var reservations: [Reservation] = []
+    private let airplane: Airplane
+    private let departure: Airport
+    private let arrival: Airport
 
-    init(number: String, date: Date, staffMembers: [Staff], reservations: [Reservation]) {
-        self.number = number
+    init(date: Date, airplane: Airplane, departure: Airport, arrival: Airport) throws {
+        self.number = Faker.generateFlightNumber()
         self.date = date
-        self.staffMembers = staffMembers
-        self.reservations = reservations
+        self.airplane = airplane
+        self.departure = departure
+        self.arrival = arrival
+
+        try airplane.assignFlight(flight: self)
+    }
+
+    public func addStaffMember(member: Staff) {
+        self.staffMembers.append(member)
+    }
+
+    public func addReservation(reservation: Reservation) {
+        self.reservations.append(reservation)
+    }
+
+    public func openFlight() {
+        self.status = FlightStatus.open
+    }
+
+    public func closeFlight() {
+        self.status = FlightStatus.closed
+    }
+
+    public func countReservations() -> Int {
+        return self.reservations.filter({ r in return r.status == ReservationStatus.confirmed })
+            .count
+    }
+
+    public func countStaffMembers() -> Int {
+        return self.staffMembers.count
     }
 
 }
--- IOS-Development/GestionDuTransport/Sources/GestionDuTransport/Models/Reservation.swift
+++ IOS-Development/GestionDuTransport/Sources/GestionDuTransport/Models/Reservation.swift
@@ -1,21 +1,34 @@
 class Reservation {
-    private let number: String
+
+    private var number: String
     private let seat: String
     private let price: Double
     private let flight: Flight
+    private unowned let passenger: Passenger
+    private(set) var status: ReservationStatus = ReservationStatus.draft
 
-    init(number: String, seat: String, price: Double, flight: Flight) {
-        self.number = number
+    init(seat: String, price: Double, flight: Flight, passenger: Passenger) {
+        self.number = Faker.generateReservationID()
         self.seat = seat
         self.price = price
         self.flight = flight
+        self.passenger = passenger
     }
 
-    public func confirm() {
-        fatalError("Not implemented")
+    public func confirm() throws {
+        guard status == ReservationStatus.draft else {
+            throw ReservationError.invalidStatus
+        }
+
+        self.status = ReservationStatus.confirmed
     }
 
-    public func cancel() {
-        fatalError("Not implemented")
+    public func cancel() throws {
+        guard status == ReservationStatus.draft else {
+            throw ReservationError.invalidStatus
+        }
+
+        self.status = ReservationStatus.cancelled
     }
+
 }
