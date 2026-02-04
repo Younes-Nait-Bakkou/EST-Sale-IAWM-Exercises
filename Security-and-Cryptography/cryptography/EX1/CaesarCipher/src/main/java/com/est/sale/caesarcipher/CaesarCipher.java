28f813c4f986e9cbbffa2c8bc27405f5934240d5
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-02-04 23:50:52 +0100

    feat: implement Caesar Cipher algorithm

--- Security-and-Cryptography/cryptography/EX1/CaesarCipher/src/main/java/com/est/sale/caesarcipher/CaesarCipher.java
+++ Security-and-Cryptography/cryptography/EX1/CaesarCipher/src/main/java/com/est/sale/caesarcipher/CaesarCipher.java
@@ -5,12 +5,47 @@
 package com.est.sale.caesarcipher;
 
 /**
- *
- * @author younes
+ * @author Younes-Nait-Bakkou
  */
 public class CaesarCipher {
 
+    static String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    // decalage
+    public static String shiftAlphabetBy(int n) {
+
+        return ALPHABET.substring(0 + n, ALPHABET.length()) + ALPHABET.substring(0, 0 + n);
+    }
+
+    // codage
+    public static String encrypt(int n, String phrase) {
+        String shiftedAlphabet = shiftAlphabetBy(n);
+        String result = "";
+
+        for (char c : phrase.toCharArray()) {
+            int indexInAlphabet = ALPHABET.indexOf(Character.toLowerCase(c));
+
+            if (indexInAlphabet == -1) {
+                result = result + c;
+                continue;
+            }
+
+            char shiftedChar = shiftedAlphabet.charAt(indexInAlphabet);
+
+            if (Character.isUpperCase(c)) {
+                result = result + Character.toUpperCase(shiftedChar);
+                continue;
+            }
+
+            result = result + shiftedChar;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(shiftAlphabetBy(1));
+        System.out.println(encrypt(1, "Hello World!"));
     }
 }
