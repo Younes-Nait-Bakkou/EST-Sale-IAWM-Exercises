4fbdd21e1d532ebf02ecb1d56982c565ea9f127d
YounesNaitBakkou <naitbakkou.younes@gmail.com>
2026-02-07 20:43:46 +0100

    feat: Add initial CalculatorWSApp project files
    Adds all necessary files for the CalculatorWSApp project, including:
    - Ant build configuration (build.xml, nbproject/*)
    - Source code for CalculatorWS (src/java/service/CalculatorWS.java)
    - Web deployment descriptors (web/WEB-INF/*.xml)
    - Web content (web/index.html)

---
+++ Backend-APIs/JEE/tp3-corrected/CalculatorWSApp/src/java/service/CalculatorWS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package service;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+
+/**
+ * @author flamer
+ */
+@WebService(serviceName = "CalculatorWS")
+public class CalculatorWS {
+
+    /** This is a sample web service operation */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "a") int a, @WebParam(name = "b") int b) {
+        return a + b;
+    }
+}
