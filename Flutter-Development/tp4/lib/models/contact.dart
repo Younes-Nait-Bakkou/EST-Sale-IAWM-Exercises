2ba6624b699974fa0466126952a68c3454056443
Younes-Nait-Bakkou <naitbakkou.younes@gmail.com>
2026-03-30 22:34:01 +0100

    feat(tp4): add contact model

---
+++ Flutter-Development/tp4/lib/models/contact.dart
@@ -0,0 +1,8 @@
+class Contact {
+  int? id;
+  String? name;
+  String? mobile;
+
+  // The constructor inside the curly braces {} means these parameters are optional and named.
+  Contact({this.id, this.name, this.mobile});
+}
